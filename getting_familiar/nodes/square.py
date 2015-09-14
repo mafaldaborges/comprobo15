cd90c0aac6a470342a8eba9b0c65afb3d26aa531
mafaldaborges <mafaldaborges94@hotmail.com>
2015-09-14 00:32:30 -0400

    added comments to code

--- getting_familiar/nodes/square.py
+++ getting_familiar/nodes/square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # import roslib; roslib.load_manifest('robot_mover')
+
+""" a program to make the robot travel in a 1m x 1m square using time to calculate distances"""
 import rospy
 
 from geometry_msgs.msg import Twist
@@ -7,19 +9,23 @@ from geometry_msgs.msg import Twist
 rospy.init_node('square')
 
 def mover():
+
     pub = rospy.Publisher('cmd_vel', Twist)
 
     twist = Twist()
+    #setting a linear travel speed to .1 m/s
     twist.linear.x = .1
     pub.publish(twist)
+    #setting the duration of sleep to allow the robot to travel forward
     rospy.sleep(10)
 
     twist.angular.z = 1
+    #having the robot turn left
     pub.publish(twist)
     rospy.sleep(1.5);
 
 
 r=rospy.Rate(10)
-
+#mover will continue to run if rospy is not shut down
 while not rospy.is_shutdown(): 
     mover()
--- getting_familiar/nodes/teleop.py
+++ getting_familiar/nodes/teleop.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+"""A code to control the direction of travel of the Neato using the keys i,k, j, and l"""
+
 import tty
 import select
 import sys
@@ -11,6 +13,7 @@ from geometry_msgs.msg import Twist
 
 rospy.init_node('teleop')
 
+#given code to pick up keystrokes
 def getKey():
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
@@ -30,30 +33,35 @@ twist = Twist()
 
 while key != '\x03':
 	key = getKey()
+	#i is pressed the robot will move forward linearly with no angular velocity until a new command is given
 	while key=="i":
 		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 		twist.linear.x = 0.1
 		twist.angular.z = 0
 		pub.publish(twist)
 		break
+	#, for backwards
 	while key==",":
 		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 		twist.linear.x = -0.1
 		twist.angular.z = 0
 		pub.publish(twist)
 		break
+	#k for stopping
 	while key=="k":
 		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 		twist.linear.x = 0
 		twist.angular.z=0
 		pub.publish(twist)
 		break
+	#j left turn
 	while key=="j":
 		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 		twist.angular.z = 0.5
 		twist.linear.z = 0
 		pub.publish(twist)
 		break
+	#l right turn
 	while key=="l":
 		pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 		twist.angular.z = -0.5
